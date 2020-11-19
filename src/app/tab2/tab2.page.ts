a8192cf04a00f36842d87607e1addea5934a3c80
HEITOR DE SOUZA GARCIA <312250@FPTE.LOCAL>
2020-11-19 13:25:02 -0300

    19/11 initial commit

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {Geolocation, Geoposition} from '@ionic-native/geolocation/ngx';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit{
 
-  constructor() {}
+  constructor(private geolocation:Geolocation) {}
 
+  ngOnInit(){
+    let watch = this.geolocation.watchPosition();
+    watch.subscribe((data:Geoposition) => {
+      console.log(data.coords);
+    },
+    (data:PositionError) =>{
+      console.log("err",data);
+    }
+    );
+  }
 }
